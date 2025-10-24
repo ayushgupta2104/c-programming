cfe70771e83e34c2b72a99cd61ae451f0850af14
Ayush Gupta <Ayush.23322@stu.upes.ac.in>
2025-10-24 20:04:29 +0530

    Add files via upload

---
+++ day 3.c
@@ -0,0 +1,28 @@
+/*#include<stdio.h>
+int main()
+{
+    int c;
+    float f;
+    printf("enter the temperature:");
+    scanf("%d", &c);
+    f=9.0/5 * c + 32;
+    printf("temperature in farenheit:%f  \n", f);
+    return 0;
+
+}*/
+/*#include<stdio.h>
+int main()
+{
+    int a, b, c;
+    printf("enter a: ");
+    scanf("%d", &a);
+    printf("enter b: ");
+    scanf("%d", &b);
+    c=a;
+    a=b;
+    b=c;
+    printf("a:%d\n", a);
+    printf("b:%d\n", b);
+    return 0;
+}*/
+
