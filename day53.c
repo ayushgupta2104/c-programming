adf650f85ff0ad80c624a230bdb9b57e06077f95
Ayush Gupta <Ayush.23322@stu.upes.ac.in>
2025-12-01 15:36:03 +0530

    Create day53.c

---
+++ day53.c
@@ -0,0 +1,42 @@
+
+#include<stdio.h>
+int main()
+{
+    int n,p=0;
+    printf("Enter the number of elements in the array:");
+    scanf("%d",&n);
+    if(n<=0)
+    {
+        printf("Number of elements cannot be non positive");
+        return 1;
+    }
+    int a[n];
+    for(int i=0;i<n;i++)
+    {
+        printf("Enter the %d element:",i+1);
+        scanf("%d",&a[i]);
+    }
+    for(int i=0;i<n;i++)
+    {
+        int sum1=0,sum2=0;
+        for(int j=0;j<i;j++)
+        {
+            sum1+=a[j];
+        }
+        
+        for(int k=i+1;k<n;k++)
+        {
+            sum2+=a[k];
+        }
+        if (sum1==sum2)
+        {
+            printf("%d",i);
+            p++;
+        }
+    }
+    if(p==0)
+    {
+        printf("-1");
+    }
+    return 0;
+}
