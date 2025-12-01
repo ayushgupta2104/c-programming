c4ecac324728886463a89d4f6bfce65b739c0c56
Ayush Gupta <Ayush.23322@stu.upes.ac.in>
2025-12-01 15:13:18 +0530

    Create day33a.c

---
+++ day33a.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+
+int binarySearch(int arr[], int n, int key) {
+    int low = 0, high = n - 1;
+
+    while (low <= high) {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] == key)
+            return mid;       // key found
+
+        else if (arr[mid] < key)
+            low = mid + 1;    // search right half
+
+        else
+            high = mid - 1;   // search left half
+    }
+
+    return -1; // key not found
+}
+
+int main() {
+    int n, key;
+
+    scanf("%d", &n);
+
+    int arr[n];
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    scanf("%d", &key);
+
+    int result = binarySearch(arr, n, key);
+
+    if (result != -1)
+        printf("Found at index %d", result);
+    else
+        printf("-1");
+
+    return 0;
+}
